ddbe26867aa2923ffbeca27baf386309c435f598
Aditya Gupta <adiguptaxd@gmail.com>
2024-07-02 12:09:09 -0400

    initial tests with working api

--- GPT.py
+++ GPT.py
@@ -5,34 +5,37 @@ class GPT:
 
     @staticmethod
     def GET_ANS_TEST(MSG):
-        LIN = ""
-        for i in range(48, 16 + 48):
-            C = chr(i)
-            LIN += C + ","
-        LIN = LIN[:-1]
-        return LIN
+        return GPT.GET_ANS(MSG)
+        # LIN = ""
+        # for i in range(48, 16 + 48):
+        #     C = chr(i)
+        #     LIN += C + ","
+        # LIN = LIN[:-1]
 
     @staticmethod
     def GET_ANS_TEST_DEG(MSG):
-        return "B.S."
+        return GPT.GET_ANS(MSG)
+        # return "B.S."
 
     @staticmethod
     def GET_ANS_TEST_PAY(MSG):
-        return "79000"
+        return GPT.GET_ANS(MSG)
+        # return "79000"
 
     @staticmethod
     def GET_ANS_TEST_X(MSG):
-        return "ABCXYZ"
+        return GPT.GET_ANS(MSG)
+        # return "ABCXYZ"
 
     @staticmethod
     def GET_ANS(MSG):
         url = "https://api.openai.com/v1/chat/completions"
-        apiKey = ""  # API key goes here
-        model = "gpt-3.5-turbo"  # current model of chatgpt api
+        api_key = ""
+        model = "gpt-3.5-turbo"
 
         try:
             headers = {
-                "Authorization": f"Bearer {apiKey}",
+                "Authorization": f"Bearer {api_key}",
                 "Content-Type": "application/json"
             }
 
@@ -45,12 +48,20 @@ class GPT:
 
             response = requests.post(url, headers=headers, data=body)
             response.raise_for_status()
-            return GPT.EXT(response.text)
+            
+            response_data = response.json()
+            
+            choice = response_data['choices']
+            within_choice = choice[0]
+            message = within_choice['message']
+            content = message['content']
+            return content.strip()
+            
         except requests.exceptions.RequestException as e:
             raise RuntimeError(e)
 
-    @staticmethod
-    def EXT(ANS):
-        start_idx = ANS.find("content") + 11
-        end_idx = ANS.find("\"", start_idx)
-        return ANS[start_idx:end_idx]
+    # @staticmethod
+    # def EXT(ANS):
+    #     start_idx = ANS.find("content") + 11
+    #     end_idx = ANS.find("\"", start_idx)
+    #     return ANS[start_idx:end_idx]
