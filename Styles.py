4a43b30e780a8578b197fad83416d6c739024e69
Aditya Gupta <adiguptaxd@gmail.com>
2024-07-19 12:04:33 -0400

    added more features

---
+++ Styles.py
@@ -0,0 +1,132 @@
+class Styles:
+    LGHT = """
+                QWidget {
+                    background-color: #F3F3F3;
+                    color: #000000;
+                    font-family: 'Consolas';
+                    font-size: 14px;
+                }
+                QLineEdit {
+                    background-color: #FFFFFF;
+                    border: 1px solid #CCCCCC;
+                    padding: 8px;
+                    border-radius: 4px;
+                    color: #000000;
+                }
+                QLineEdit:focus {
+                    border: 1px solid #2FB7E0;
+                }
+                QTextEdit {
+                    background-color: #FFFFFF;
+                    border: 1px solid #CCCCCC;
+                    padding: 8px;
+                    border-radius: 4px;
+                    color: #000000;
+                }
+                QPushButton {
+                    background-color: #2FB7E0;
+                    border: 2px solid #279ABA;
+                    color: #000000;
+                    padding: 4px;
+                    border-radius: 4px;
+                    font-weight: bold;
+                }
+                QPushButton:hover {
+                    background-color: #279ABA;
+                }
+                QPushButton:pressed {
+                    background-color: #279ABA;
+                }
+                QPushButton:disabled {
+                    background-color: #BBBBBB;
+                    border: 2px solid #999999;
+                }
+            """
+
+    DARK =  """
+                QWidget {
+                    background-color: #2E2E2E;
+                    color: #FFFFFF;
+                    font-family: 'Consolas';
+                    font-size: 14px;
+                }
+                QLineEdit {
+                    background-color: #3E3E3E;
+                    border: 1px solid #666666;
+                    padding: 8px;
+                    border-radius: 4px;
+                    color: #FFFFFF;
+                }
+                QLineEdit:focus {
+                    border: 1px solid #4DDDA6;
+                }
+                QTextEdit {
+                    background-color: #3E3E3E;
+                    border: 1px solid #666666;
+                    padding: 8px;
+                    border-radius: 4px;
+                    color: #FFFFFF;
+                }
+                QPushButton {
+                    background-color: #4DDDA6;
+                    border: 2px solid #43BA8A;
+                    color: #000000;
+                    padding: 2px;
+                    border-radius: 4px;
+                    font-weight: bold;
+                }
+                QPushButton:hover {
+                    background-color: #43BA8A;
+                }
+                QPushButton:pressed {
+                    background-color: #43BA8A;
+                }
+                QPushButton:disabled {
+                    background-color: #BBBBBB;
+                    border: 2px solid #999999;
+                }
+            """
+
+    MISC_0 =  """
+                QWidget {
+                    background-color: #3E3E3E;
+                    color: #FFFFFF;
+                    font-family: 'Consolas';
+                    font-size: 14px;
+                }
+                QLineEdit {
+                    background-color: #4E4E4E;
+                    border: 1px solid #666666;
+                    padding: 8px;
+                    border-radius: 4px;
+                    color: #FFFF88;
+                }
+                QLineEdit:focus {
+                    border: 1px solid #DDCC00;
+                }
+                QTextEdit {
+                    background-color: #4E4E4E;
+                    border: 1px solid #666666;
+                    padding: 8px;
+                    border-radius: 4px;
+                    color: #FFFF88;
+                }
+                QPushButton {
+                    background-color: #DDCC00;
+                    border: 2px solid #CCBB00;
+                    color: #000000;
+                    padding: 2px;
+                    border-radius: 4px;
+                    font-weight: bold;
+                }
+                QPushButton:hover {
+                    background-color: #CCBB00;
+                }
+                QPushButton:pressed {
+                    background-color: #CCBB00;
+                }
+                QPushButton:disabled {
+                    background-color: #BBBBBB;
+                    border: 2px solid #999999;
+                }
+            """
